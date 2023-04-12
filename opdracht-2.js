9945f3be95c7c48335efccfad51d07965372a216
IlsevNierop <ilsevannierop@gmail.com>
2023-04-12 10:14:34 +0200

    tm opdracht 3 af

--- opdracht-2.js
+++ opdracht-2.js
@@ -2,10 +2,30 @@
 // Tip: wanneer we meerdere waardes uit een array willen terugbrengen tot één getal of één string, gebruik je hier gewoon een oude vertrouwde for-loop voor!
 //
 //     Opdracht 2a: Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
-
-
+let soldTvs = 0;
+for (let i = 0; i < inventory.length; i++) {
+    soldTvs += inventory[i].sold;
+}
+console.log(soldTvs);
 
 // Opdracht 2b: Zorg ervoor dat dit aantal in het groen wordt weergegeven op de pagina.
+
+const soldItemsDashboard = document.getElementById("items-sold");
+soldItemsDashboard.textContent = `${soldTvs}`;
+
 //     Opdracht 2c: Hoeveel tv's heeft Tech It Easy ingekocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+let originalStock = 0;
+for (let i = 0; i < inventory.length; i++) {
+    originalStock += inventory[i].originalStock;
+}
+console.log(originalStock);
+
 // Opdracht 2d: Zorg ervoor dat dit aantal in het blauw wordt weergegeven op de pagina.
+
+const originalStockDashboard = document.getElementById("original-stock");
+originalStockDashboard.textContent = `${originalStock}`;
+
 //     Opdracht 2e: Geef in het rood weer hoeveel tv's er nog verkocht moeten worden.
+
+const inventoryDashboard = document.getElementById("inventory");
+inventoryDashboard.textContent = `${originalStock - soldTvs}`;
---
+++ opdracht-3.js
@@ -0,0 +1,15 @@
+// Opdracht 3 - Array methoden en functies
+// Opdracht 3a: Gebruik een array-methode om alle tv merken (zoals Philips, NIKKEI, etc.) in een lijst op de pagina weer te geven. Zorg ervoor dat dit ook zou werken als we 200 tv's in onze array zouden hebben staan. Dat er dubbele namen in zitten, is niet erg.
+
+// const listBrands = document.getElementById("list-brands");
+//
+// const outputTvBrands = inventory.map((inventory) => {listBrands.innerHTML += `<li>${inventory.brand}</li>`});
+
+// Opdracht 3b: Schrijf de code uit 3a om naar een functie die een array met tv-objecten verwacht. Het is handig om onze scripts als functies op te zetten, zodat we ze gemakkelijk kunnen hergebruiken. Tip: vergeet deze functie -declaratie niet aan te roepen!
+
+function generateListTvBrands (arrayTvs) {
+    const listBrands = document.getElementById("list-brands");
+    arrayTvs.map((tv) => {listBrands.innerHTML += `<li>${tv.brand}</li>`});
+}
+
+generateListTvBrands(inventory);
